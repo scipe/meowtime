07f4572ccc76eab5bf0da5e5d9dc2c5708a741bb
miarur <deadheartedfo@yahoo.com>
2022-08-17 19:31:09 +0300

    fix (client): fix app.ts

--- stack/client/src/App.tsx
+++ stack/client/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 import Header from './components/layouts/Header/Header';
 
 function App() {
   return (
-    <Header />
+    <Auth0Provider
+      domain="meowtime.eu.auth0.com"
+      clientId="kifi54TMhcXzodgIzHSHT3S13dpPhBOU"
+      redirectUri={window.location.origin}
+    >
+      <Header />
+    </Auth0Provider>
   );
 }
 
