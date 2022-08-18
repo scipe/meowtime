64c25a37e3aafcda0d0969971e3f5ed114a4516f
miarur <deadheartedfo@yahoo.com>
2022-08-18 18:12:19 +0300

    feat( client ): add block with review information

--- stack/client/src/pages/Splash/Splash.tsx
+++ stack/client/src/pages/Splash/Splash.tsx
@@ -3,6 +3,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import Header from '../../layouts/Header/Header';
 import Capabilities from '../../layouts/splash/Capabilities/Capabilities';
 import SectionAbout from '../../layouts/splash/SectionAbout/SectionAbout';
+import SectionReview from '../../layouts/splash/SectionReview/SectionReview';
 
 const Splash = () => {
   return (
@@ -16,6 +17,7 @@ const Splash = () => {
       </Auth0Provider>
       <Capabilities />
       <SectionAbout />
+      <SectionReview />
     </>
   );
 };
