8a646ced10325456e009519f0c18fc441d7c8114
Yury Vinogradov <41233522+yuvinogradov@users.noreply.github.com>
2022-08-21 13:18:25 +0300

    chore (api): add scope check to routes

--- stack/api/server.js
+++ stack/api/server.js
@@ -2,10 +2,25 @@ import express from 'express';
 import pets from './routes/pets.route.js';
 import { expressjwt } from 'express-jwt';
 import jwks from 'jwks-rsa';
+import dotenv from 'dotenv';
+import { auth, requiredScopes } from 'express-oauth2-jwt-bearer';
+import cors from 'cors';
 
+dotenv.config();
 const app = express();
 
-var jwtCheck = expressjwt({
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+if (!process.env.ISSUER_BASE_URL || !process.env.AUDIENCE) {
+  throw 'Make sure you have ISSUER_BASE_URL, and AUDIENCE in your .env file';
+}
+
+app.use(cors(corsOptions));
+const checkJwt = auth();
+
+const jwtCheck = expressjwt({
   secret: jwks.expressJwtSecret({
     cache: true,
     rateLimit: true,
@@ -22,10 +37,12 @@ app.use(express.json());
 
 app.use('/', pets);
 app.use('/version/', pets);
-app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
+app.use('/authorizedpets/', pets);
+app.use('/notauthorizedpets/', pets);
 
 app.get('/authorized', function (req, res) {
   res.send('Secured Resource');
 });
+app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
 
 export default app;
